e3542d49d122105c3acdc2d73947e5566ea61456
Kamil Mieczkowski <kamil.mieczkowski7@gmail.com>
2024-10-19 20:08:28 +0200

    fix navbar responsiveness

--- CinemaFrontend/src/Components/NavigateComponent.jsx
+++ CinemaFrontend/src/Components/NavigateComponent.jsx
@@ -3,57 +3,69 @@ import { Link } from "react-router-dom";
 import "../styles/navigate.css";
 import Logo from "../images/logo.png";
 import { AiOutlineMenuFold } from "react-icons/ai";
+import { MdCancel } from "react-icons/md";
 
 function NavigateComponent() {
+  const [isPanelVisible, setPanelVisible] = useState(false);
+
   useEffect(() => {
     const allLi = document.querySelector(".menuList").querySelectorAll("li");
 
-    function changeManuActive() {
+    function changeMenuActive() {
       allLi.forEach((n) => n.classList.remove("active"));
       this.classList.add("active");
     }
 
-    allLi.forEach((n) => n.addEventListener("click", changeManuActive));
+    allLi.forEach((n) => n.addEventListener("click", changeMenuActive));
 
     allLi[0].classList.add("active");
   }, []);
 
+  const togglePanel = () => {
+    setPanelVisible(!isPanelVisible);
+  };
+
   return (
-    <div className="navigateComponent">
-      <div className="logo">
-        <img src={Logo} alt="" />
-        <p>CinemaManagment</p>
-      </div>
+      <div className="navigateComponent">
 
-      <div className="panel">
-        <nav className="menuList">
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/movies">Movies</Link>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Help</a>
-            </li>
-          </ul>
-        </nav>
-        <div className="profileBox">
-          <img src="" alt="" />
-          <p>Imie Nazwisko</p>
-          <i>
-            <AiOutlineMenuFold />
-          </i>
+        <i className="iMenu" onClick={togglePanel}>
+          {isPanelVisible ? <MdCancel /> : <AiOutlineMenuFold/>}
+        </i>
+        <div className="logo">
+          <img src={Logo} alt=""/>
+          <p>CinemaManagment</p>
         </div>
-      </div>
 
-      <div className="bottom"></div>
-    </div>
+        <div className={`panel ${isPanelVisible ? "visible" : ""}`}>
+
+          <nav className="menuList">
+            <ul>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/movies">Movies</Link>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Help</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="profileBox">
+            <img src="" alt=""/>
+            <p>Imie Nazwisko</p>
+            <i>
+              <AiOutlineMenuFold/>
+            </i>
+          </div>
+        </div>
+
+        <div className="bottom"></div>
+      </div>
   );
 }
 
-export { NavigateComponent };
+export {NavigateComponent};
