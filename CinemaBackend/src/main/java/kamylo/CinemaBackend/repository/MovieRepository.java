f3308226b9e51d7238727be4a36b03b58ef955e1
KamYloo <kamil.mieczkowski7@gmail.com>
2026-05-20 18:03:08 +0200

    add EntityGraph for eager user fetching with Redis cach

--- CinemaBackend/src/main/java/kamylo/CinemaBackend/repository/MovieRepository.java
+++ CinemaBackend/src/main/java/kamylo/CinemaBackend/repository/MovieRepository.java
@@ -1,6 +1,7 @@
 package kamylo.CinemaBackend.repository;
 
 import kamylo.CinemaBackend.model.Movie;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,8 +11,10 @@ import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    @EntityGraph(attributePaths = {"user"})
     List<Movie> findAllByOrderByIdDesc();
 
+    @EntityGraph(attributePaths = {"user"})
     @Query("select m from Movie m where lower(m.title) like lower(concat('%',:title,'%'))")
     List<Movie> findByTitle(@Param("title") String title);
 }
